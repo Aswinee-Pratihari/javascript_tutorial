d102b9a8a87f765bde673b006b05a69301162a91
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-12-25 00:16:46 +0530

    Datatypes

---
+++ 2_datatypes.js
@@ -0,0 +1,25 @@
+let name = "Aswinee";
+let age = 18;
+let isLoggedIn = false;
+let state;
+let sym = Symbol("foo");
+const detail = {
+  name,
+  age,
+  isLoggedIn,
+  state,
+  sym,
+};
+
+/*
+number 
+bigInt
+string
+boolean
+null
+undefined
+symbol
+object
+*/
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
